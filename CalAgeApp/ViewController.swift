7a2a81b10a0e8d9926c92c56707685775a09c57b
marwa <marwa@marwas-mac.local>
2018-06-25 04:12:15 -0700

    first

--- CalAgeApp/ViewController.swift
+++ CalAgeApp/ViewController.swift
@@ -1,25 +1,51 @@
-//
-//  ViewController.swift
-//  CalAgeApp
-//
-//  Created by marwa on 3/29/18.
-//  Copyright © 2018 marwa. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  , UIPickerViewDelegate , UIPickerViewDataSource{
+    
+    var gender = ["Meal","Femal"]
+    @IBOutlet weak var ageText: UITextField!
+    @IBOutlet weak var heightText: UITextField!
+    @IBOutlet weak var Result: UITextField!
+    @IBOutlet weak var genderPicer: UIPickerView!
+    @IBOutlet weak var selectgender: UILabel!
+    var selectgen = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    genderPicer.dataSource = self
+    genderPicer.delegate = self
+        }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return gender.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        selectgen  = row
+        return gender[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectgender.text = gender[row]
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func BestWeight(_ sender: UIButton) {
+        let age:Int = Int(ageText.text!)!
+        let hieght:Int = Int(heightText.text!)!
+        var result  = hieght*10 - (age/5)
+        if selectgen == 0 {
+            result += 10
+        }else{
+            result -= 10
+        }
+        Result.text = "the best weight is \(result)"
+        
+        
+    }
+    
 }
 
